563b8dcf53d8f4186add647d0f8dd39008309f8a
puppe1990 <matheus.puppe90@hotmail.com>
2022-12-18 19:13:28 -0300

    refactor class

--- eft.rb
+++ eft.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class EFT
   TAPPING_POINTS = [
     'Karate chop',
@@ -9,49 +11,68 @@ class EFT
     'Chin',
     'Collarbone',
     'Underarm'
-  ]
+  ].freeze
 
   DEFAULT_PHRASES = [
     'Even though I have this [issue], I deeply and completely accept myself.',
     'I am willing to let go of this [issue].',
     'I choose to release this [issue].',
     'I am free of this [issue].'
-  ]
+  ].freeze
 
   FINAL_PHRASES = [
-    'I am worthy and deserving of [positive outcome or goal].',
-    'I am confident and capable of [positive outcome or goal].',
-    'I am open to feeling [positive emotion] in place of this [issue].'
-  ]
+    'I am worthy and deserving of [positive emotion or goal].',
+    'I am confident and capable of [positive emotion or goal].',
+    'I am open to feeling [positive emotion or goal] in place of this [issue].'
+  ].freeze
+
+  def self.ask_feeling
+    print 'On a scale from 0 to 10, how do you feel now? '
+    gets.to_i
+  end
+
+  def self.get_positive_emotion_or_goal
+    print 'Enter a positive emotion or goal: '
+    gets.chomp
+  end
+
+  def self.get_issue
+    print 'Enter an issue or emotion to focus on: '
+    gets.chomp
+  end
+
+  def self.tap_points(issue, phrases, final_phrase = nil)
+    TAPPING_POINTS.each do |point|
+      puts "Tap on the \e[32m#{point}\e[0m while focusing on the following phrase: '\e[32m#{final_phrase || phrases.sample.gsub('[issue]', issue)}\e[0m'"
+      puts 'Take a deep breath and tap 5-7 times on each point.'
+      phrase = phrases.sample.gsub('[issue]', issue)
+      if point == 'Karate chop'
+        3.times { puts "\e[32m#{phrase}\e[0m" }
+      else
+        puts "\e[32m#{phrase}\e[0m"
+      end
+      print 'Press enter to continue or type "exit" to end the session: '
+      input = gets.chomp
+      system('clear') # This will clear the console
+      return if input == 'exit'
+    end
+  end
 
   def self.tap(issue, phrases = DEFAULT_PHRASES)
     feeling = nil
     until feeling == 0
-      print 'On a scale from 0 to 10, how do you feel now? '
-      feeling = gets.to_i
-      break if feeling == 0
-
-      TAPPING_POINTS.each do |point|
-        puts "Tap on the \e[32m#{point}\e[0m while focusing on the issue: '\e[32m#{issue}\e[0m'"
-        puts 'Take a deep breath and tap 5-7 times on each point.'
-        puts 'As you tap, repeat one of the following phrases to yourself:'
-        phrase = phrases.sample.gsub('[issue]', issue)
-        if point == 'Karate chop'
-          3.times { puts "\e[32m#{phrase}\e[0m" }
-        else
-          puts "\e[32m#{phrase}\e[0m"
-        end
-
-        print 'Press enter to continue or type "exit" to end the session: '
-        input = gets.chomp
-        system('clear')  # This will clear the console
-        break if input == 'exit'
+      feeling = ask_feeling
+      if feeling == 0
+        positive_emotion_or_goal = get_positive_emotion_or_goal
+        final_phrase = FINAL_PHRASES.sample.gsub('[positive emotion or goal]', positive_emotion_or_goal)
+        tap_points(issue, phrases, final_phrase)
+        puts "\e[32m#This is the end!\e[0m"
+        break
       end
+      tap_points(issue, phrases)
     end
   end
 end
 
-print 'Enter an issue or emotion to focus on: '
-issue = gets.chomp
-
+issue = EFT.get_issue
 EFT.tap(issue)
