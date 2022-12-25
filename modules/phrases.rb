60fdfd1a59cf722af97c8796d37dd78d3ceeea4e
puppe1990 <matheus.puppe90@hotmail.com>
2022-12-25 12:18:22 -0300

    create module to phrases

---
+++ modules/phrases.rb
@@ -0,0 +1,51 @@
+module Phrases
+  TAPPING_POINTS = [
+    'Karate chop',
+    'Top of head',
+    'Eyebrow',
+    'Side of eye',
+    'Under eye',
+    'Under nose',
+    'Chin',
+    'Collarbone',
+    'Chest',
+    'Underarm'
+  ].freeze
+
+  DEFAULT_PHRASES = [
+    'Even though I have this [issue], I deeply and completely accept myself.',
+    'I am willing to let go of this [issue].',
+    'I choose to release this [issue].',
+    'I am free of this [issue].',
+    'I am letting go of this [issue].',
+    'I am releasing this [issue].',
+    'I am moving on from this [issue].',
+    'I am leaving this [issue] behind.',
+    'I am no longer held back by this [issue].',
+    'I am stronger than this [issue].'
+  ].freeze
+
+  FINAL_PHRASES = [
+    'I am worthy and deserving of [positive emotion or goal].',
+    'I am confident and capable of [positive emotion or goal].',
+    'I am open to feeling [positive emotion or goal] in place of this [issue].',
+    'I am grateful for [positive emotion or goal] in my life.',
+    'I am ready to embrace [positive emotion or goal].',
+    'I am filled with [positive emotion or goal].',
+    'I am worthy of feeling [positive emotion or goal].',
+    'I am deserving of [positive emotion or goal].',
+    'I am worthy and deserving of feeling [positive emotion or goal].',
+    'I am open to experiencing [positive emotion or goal] in place of this [issue].'
+  ].freeze
+
+  CUSTOM_PHRASES = ['Even though I struggle with financial abundance, I deeply and completely accept myself.',
+                    'I have a hard time believing I deserve financial abundance.',
+                    "I feel like I'm not worthy of abundance.",
+                    "I don't know how to attract abundance.",
+                    "I'm scared that I'll never have enough money.",
+                    "I'm always worried about money.",
+                    "I don't trust that I can have abundance.",
+                    "I don't believe I deserve abundance.",
+                    "I'm worried that I'll never be able to afford the things I want.",
+                    "I'm always struggling to make ends meet."].freeze
+end 
