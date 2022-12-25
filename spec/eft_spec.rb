c8729143b0897bd416bfa1cdf67b3e3d87194612
puppe1990 <matheus.puppe90@hotmail.com>
2022-12-25 12:05:54 -0300

    add new tests

--- spec/eft_spec.rb
+++ spec/eft_spec.rb
@@ -2,37 +2,74 @@ require 'rspec'
 require_relative '../eft'
 
 describe EFT do
+  describe '.ask_feeling' do
+    it 'prompts the user to enter a feeling on a scale from 0 to 10' do
+      allow(EFT).to receive(:gets).and_return('5')
+      expect { EFT.ask_feeling }.to output("On a scale from 0 to 10, how do you feel now? ").to_stdout
+    end
+
+    it 'returns the user input as an integer' do
+      allow(EFT).to receive(:gets).and_return('5')
+      expect(EFT.ask_feeling).to eq(5)
+    end
+  end
+
+  describe '.get_positive_emotion_or_goal' do
+    it 'prompts the user to enter a positive emotion or goal' do
+      allow(EFT).to receive(:gets).and_return('happiness')
+      expect { EFT.get_positive_emotion_or_goal }.to output("Enter a positive emotion or goal: ").to_stdout
+    end
+
+    it 'returns the user input as a string' do
+      allow(EFT).to receive(:gets).and_return('happiness')
+      expect(EFT.get_positive_emotion_or_goal).to eq('happiness')
+    end
+  end
+
+  describe '.get_issue' do
+    it 'prompts the user to enter an issue or emotion to focus on' do
+      allow(EFT).to receive(:gets).and_return('anxiety')
+      expect { EFT.get_issue }.to output("Enter an issue or emotion to focus on: ").to_stdout
+    end
+
+    it 'returns the user input as a string' do
+      allow(EFT).to receive(:gets).and_return('anxiety')
+      expect(EFT.get_issue).to eq('anxiety')
+    end
+  end
+
+  describe '.tap_points' do
+    it 'displays tapping points and phrases to the user' do
+      allow(EFT).to receive(:gets).and_return('exit')
+      expect { EFT.tap_points('anxiety', EFT::DEFAULT_PHRASES) }.to output(/Tap on the .* while focusing on the following issue: 'anxiety'/).and_output { |output| expect(output.strip).to match(/Tap on the .* while focusing on the following issue: 'anxiety'/) }
+    end    
+
+    it 'exits the session if the user types "exit"' do
+      allow(EFT).to receive(:gets).and_return('exit')
+      expect(EFT.tap_points('anxiety', EFT::DEFAULT_PHRASES)).to be_nil
+    end
+  end
+
   describe '.tap' do
-
-    xit 'outputs a phrase to repeat while tapping' do
-      # Stub the first call to gets with a feeling level of 10
-      allow(EFT).to receive(:gets).and_return('10')
-      # Stub the second call to gets with a feeling level of 0
-      allow(EFT).to receive(:gets).and_return('0')
-      # Stub all subsequent calls to gets with an empty string
-      allow(EFT).to receive(:gets).and_return('')
-      expect { EFT.tap('anxiety') }.to output(/Even though I have this anxiety, I deeply and completely accept myself./).to_stdout
-    end    
-
-    it 'outputs instructions for each tapping point' do
-      allow(EFT).to receive(:gets).and_return('10', '0')
-      expect { EFT.tap('anxiety') }.to output(/Tap on the/).to_stdout
-    end    
-
-    it 'exits the loop if the user types "exit"' do
-      allow(EFT).to receive(:gets).and_return('10', 'exit')
-      expect { EFT.tap('anxiety') }.not_to output(/I am worthy and deserving of/).to_stdout
+    it 'starts a tapping session and prompts the user to enter their feelings' do
+      allow(EFT).to receive(:gets).and_return('5', 'exit')
+      expect { EFT.tap('anxiety', EFT::DEFAULT_PHRASES) }.to output(/On a scale from 0 to 10, how do you feel now?/).to_stdout
     end
 
-    it 'uses the default phrases if no phrases are provided' do
-      allow(EFT).to receive(:gets).and_return('10', '0')
-      expect { EFT.tap('anxiety') }.to output(/Even though I have this anxiety, I deeply and completely accept myself./).to_stdout
+    it 'exits the session if the user feels a 0 on the scale' do
+      allow(EFT).to receive(:gets).and_return('0', 'exit')
+      expect(EFT.tap('anxiety', EFT::DEFAULT_PHRASES)).to be_nil
     end
 
-    it 'uses the provided phrases if given' do
-      custom_phrases = ['Custom phrase 1', 'Custom phrase 2']
-      allow(EFT).to receive(:gets).and_return('10', '0')
-      expect { EFT.tap('anxiety', custom_phrases) }.to output(/Custom phrase/).to_stdout
+    it 'asks the user for a positive emotion or goal if the feeling is less than 10' do
+      allow(EFT).to receive(:gets).and_return('5', 'happiness', 'exit')
+      expect { EFT.tap('anxiety', EFT::DEFAULT_PHRASES) }.to output(/Enter a positive emotion or goal: /).to_stdout
+    end
+
+    it 'taps on the points and displays the phrases to the user' do
+      allow(EFT).to receive(:gets).and_return('5', 'happiness', 'exit')
+      expect { EFT.tap('anxiety', EFT::DEFAULT_PHRASES) }.to output(/Tap on the .* while focusing on the following issue: 'anxiety'/).to_stdout
     end
   end
 end
+
